ed38fef03c9029552bd49a55ccd18b1ea40eb02b
Hannah Squier <hannahsquier@gmail.com>
2016-09-16 16:12:07 -0700

    set up post request

--- script.js
+++ script.js
@@ -4,9 +4,15 @@ function Puppy(name, breed, created_at) {
   this.created_at = created_at;
 }
 
+function Breed(name, id){
+  this.name = name
+  this.id = id
+}
+
 var model = {
   init: function() {
     this.puppies = [];
+    this.breeds = [];
   },
 
   addPuppyToList: function(puppyName, puppyBreed, created_at) {
@@ -14,15 +20,26 @@ var model = {
   },
 
   createPuppyList: function(response) {
+    model.puppies = [];
     for(var puppy in response) {
-      model.addPuppyToList(response[puppy].name, response[puppy].breed.name, response[puppy].created_at);
+      model.addPuppyToList(response[puppy].name, response[puppy].breed, response[puppy].created_at);
+    }
+  },
+
+  createBreedList: function(response) {
+    model.breeds = [];
+    for(var breed in response) {
+     model.breeds.push(new Breed(response[breed].name, response[breed].id));
     }
   }
 };
 
 var controller = {
   init: function() {
+    API.getBreeds();
+    API.getPuppyList();
     view.submitButtonListener();
+    view.refreshListener();
     model.init();
   },
 
@@ -30,10 +47,11 @@ var controller = {
     e.preventDefault();
 
     var puppyName = view.getPuppyName();
-    var puppyBreed = view.getPuppyBreed();
+    var breedID = view.getPuppyBreedID();
 
     if(puppyName) {
-      model.addPuppyToList(puppyName, puppyBreed);
+      API.sendPuppy(puppyName, breedID)
+     // model.addPuppyToList(puppyName, puppyBreed);
     }
   }
 };
@@ -45,22 +63,34 @@ var view = {
     $("#submit-button").on("click", controller.submitButtonHandler)
   },
 
+  refreshListener: function() {
+    $("#refresh").on("click", API.getPuppyList)
+  },
+
   getPuppyName: function() {
     return $("#puppy-name").val();
   },
 
-  getPuppyBreed: function() {
+  getPuppyBreedID: function() {
     return $("select").val();
   },
 
-  render: function(puppyList) {
+  render: function(puppyList, breedList) {
+    $select = $("select")
+    for(b in breedList) {
+      $option = $("<option>")
+      $option.text(breedList[b].name)
+            .val(breedList[b].id)
+      $select.append($option)
+    }
+
     $list = $('#puppy-list');
     var now = new Date();
 
     for (var puppy in puppyList) {
       var creationDate = new Date(puppyList[puppy].created_at);
       $newli = $('<li></li>')
-        .text(puppyList[puppy].name + " (" + puppyList[puppy].breed + "), created " + ((now - creationDate) / 1000 / 60)  + " minutes ago.");
+        .text(puppyList[puppy].name + " (" + puppyList[puppy].breed.name + "), created " + ((now - creationDate) / 1000 / 60)  + " minutes ago.");
       $list.append( $newli );
     }
   }
@@ -69,28 +99,41 @@ var view = {
 var API = {
   getPuppyList: function() {
     var puppyPromise =  $.ajax({ url: "https://ajax-puppies.herokuapp.com/puppies.json",
-                                success: function(response) { 
-                                  model.createPuppyList(response); 
-                                  view.render(model.puppies);
-                                } 
+                                success: function(response) {
+                                  model.createPuppyList(response);
+                                  view.render(model.puppies, model.breeds);
+                                }
                               });
 
     return puppyPromise;
-  }
+  },
 
-  sendPuppy: function(puppy) {
-    
+  getBreeds: function() {
+    var breedPromise =  $.ajax({ url: "https://ajax-puppies.herokuapp.com/breeds.json",
+                                success: function(response) {
+                                  model.createBreedList(response);
+                                  // view.render(model.puppies);
+                                }
+                              });
+  },
+
+  sendPuppy: function(name, breedID) {
+
     options = {
       url: 'https://ajax-puppies.herokuapp.com/puppies.json',
       method: 'POST',
-      contentType: "application/x-www-form-urlencoded",
+      contentType: 'application/json',
       dataType: "json",
-      data: JSON.stringify(puppy);
+      data: JSON.stringify({name: name, breed_id: breedID}),
+      success: function() { alert("Thanks for your puppy submission!")},
+      error: function() { alert("Oops something went wrong!")}
 
     }
 
-    var puppyPromise = 
-  };
+    $.ajax(options)
+
+    //var puppyPromise =
+  }
 
 
 };
